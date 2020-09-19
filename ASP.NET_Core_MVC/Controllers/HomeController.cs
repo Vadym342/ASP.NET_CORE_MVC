ba247bcc0c11456bceb4e4e83ae39501517a8725
BLABLABLA <vadimca1.boyarchuc@gmail.com>
2020-09-19 14:45:33 +0300

    Add index page

---
+++ ASP.NET_Core_MVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ASP.NET_Core_MVC.Data.Interfaces;
+using ASP.NET_Core_MVC.Data.Models;
+using ASP.NET_Core_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly IAllCars _carRep;
+      
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+          
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel {
+             favCars= _carRep.getFavCars
+            };
+
+            return View(homeCars);
+        }
+
+
+
+
+    }
+}
---
+++ ASP.NET_Core_MVC/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ASP.NET_Core_MVC.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> favCars { get; set; }
+
+    }
+}
