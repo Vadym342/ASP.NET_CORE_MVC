9efc916a28968a3bcf0a09ed99d554d26dda8af6
BLABLABLA <vadimca1.boyarchuc@gmail.com>
2020-09-24 23:46:02 +0300

    Add model

---
+++ ASP.NET_Core_MVC/Data/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC.Data.Models
+{
+    public class Comment
+    {
+        int id { get; set; }
+
+        string message { get; set; }
+
+
+    }
+}
