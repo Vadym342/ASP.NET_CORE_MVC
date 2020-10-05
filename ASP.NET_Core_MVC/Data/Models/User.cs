7727c893d17cac8d3a52d8a9e76bfe1a262fe9e0
BLABLABLA <vadimca1.boyarchuc@gmail.com>
2020-10-05 20:08:27 +0300

    Add controllers

---
+++ ASP.NET_Core_MVC/Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC.Data.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string phoneNumber { get; set; }
+        public int age { get; set; }
+
+
+    }
+}
